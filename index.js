c3522546085ff51c93ab357a17454f33813b8f48
Corey Cook <cookc7438@rwps.org>
2020-02-14 16:08:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = [ "Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(kittens) {
-  append(kittens)
+  push.kittens('new cat')
   return kittens
 }
